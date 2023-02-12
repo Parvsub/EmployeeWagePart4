1171fbea38364cf0ceaa71f13ad981eb401ec705
Parvsub <Praveenboda421@gmail.com>
2023-02-12 20:02:22 +0530

    Added:Employee PartTime is added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,22 +1,24 @@
 package EmployeeWagePartfour;
 
 public class EmployeeWage {
+            void AddPartTime(int FullTime, int PartTime, int WagePerHour, int EmployeeHours, int EmployeeWage){
+            double employeeCheck = Math.floor(Math.random() * 10) % 3;
+            if (employeeCheck == FullTime) {
+                EmployeeHours = 8;
+            }
+            else if (employeeCheck == PartTime){
+                EmployeeHours = 4;
+            }
+            else {
+                EmployeeHours = 0;
+            }
+            EmployeeWage = EmployeeHours * WagePerHour;
+            System.out.println("employeeWage : "+EmployeeWage);
+        }
 
-    void DailyWage(int Full_Time, int wagePerHour,int employeeHours,int employeeWage){
-        double employeeCheck = Math.floor(Math.random() * 10)%2 ;
-        if (employeeCheck == Full_Time) {
-            employeeHours = 8;
+            public static void main(String[] args) {
+            EmployeeWage partTimeEmployee = new EmployeeWage();
+            partTimeEmployee.AddPartTime(2,1,20,0,0);
         }
-        else {
-            employeeHours = 0;
-        }
-        employeeWage = employeeHours * wagePerHour ;
-        System.out.println("dailyEmployeeWage: "+employeeWage);
     }
 
-    public static void main(String[] args){
-        EmployeeWage employeeDailywage = new EmployeeWage();
-        employeeDailywage.DailyWage(1,20,0,0);
-    }
-
-}
