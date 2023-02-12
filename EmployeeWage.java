a1f63a166ea71718217eb190d90481cf496ecc8a
Parvsub <Praveenboda421@gmail.com>
2023-02-12 19:48:06 +0530

    Added:Employee Attendance

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,18 @@
 package EmployeeWagePartfour;
 
 public class EmployeeWage {
-            public static void main(String[] args) {
-            System.out.println("Welcome to EmployeeWage computation Problem");
+             void Attendance(int Full_time) {
+            // To get random number and check the attendance
+            double employeeCheck = (Math.floor(Math.random() * 10) % 2);
+            if (employeeCheck == Full_time) {
+                System.out.println("Employee is Present");
+            } else {
+                System.out.println("Employee is Absent");
+            }
+        }
+
+        public static void main(String[] args) {
+            EmployeeWage attendance = new EmployeeWage();
+            attendance.Attendance(1);
         }
     }
